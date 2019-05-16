57c4b177c8503ef1de291d256b93ee952c546c8f
Nick Do <nkhl_linh@yahoo.com>
2019-05-16 18:02:45 +1200

    MINOR CHANGE: added auto-complete component

---
+++ .storybook/Autocomplete.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Box, Text, Icon } from "../src";
+import catNames from "cat-names";
+import Component from "@reach/component-component";
+import { Autocomplete, LabelAuto, InputAuto, MenuAuto, ItemAuto } from "../src";
+
+const cats = catNames.all;
+const match = (item, value) => item.includes(value);
+
+storiesOf("Autocomplete", module).add("default", () => (
+  <Component
+    initialState={{ value: "" }}
+    children={({ state, setState }) => (
+      <Box>
+        <Autocomplete
+          onChange={item => {
+            setState({ value: item });
+          }}
+          match={match}
+        >
+          <LabelAuto mb={1}>Cat</LabelAuto>
+          <InputAuto/>
+          <MenuAuto>
+            {cats.map(cat => (
+              <ItemAuto key={cat} item={cat}>
+                <Icon name="Pin" color="blue" mr={2}/>
+                <Text fontSize={0}>{cat}</Text>
+              </ItemAuto>
+            ))}
+          </MenuAuto>
+        </Autocomplete>
+        <Text my={2}>
+          This text should be covered up by the Autocomplete. Menu when open.
+        </Text>
+        <Text my={2}>
+          The current value is <code>{state.value}</code>
+        </Text>
+      </Box>
+    )}
+  />
+));
---
+++ src/Autocomplete.js
@@ -0,0 +1,141 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Downshift from "downshift";
+import styled from "styled-components";
+import {
+  Flex,
+  Card,
+  Label as PclnLabel,
+  Input as PclnInput
+} from "./";
+import { themeGet } from "styled-system";
+
+export const AutocompleteContext = React.createContext();
+
+export const withAutocomplete = (Component, mapProps) =>
+  React.forwardRef((props, ref) => (
+    <AutocompleteContext.Consumer
+      children={state => (
+        <Component ref={ref} {...props} {...mapProps({ ...props, ...state })} />
+      )}
+    />
+  ));
+
+export const LabelAuto = withAutocomplete(PclnLabel, ({ getLabelProps }) =>
+  getLabelProps()
+);
+LabelAuto.isLabel = true;
+
+export const InputAuto = withAutocomplete(PclnInput, ({ getInputProps }) =>
+  getInputProps()
+);
+InputAuto.isField = true;
+
+const MenuCard = styled(Card)`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  right: 0;
+  max-height: ${props => props.height};
+  overflow-y: auto;
+`;
+
+MenuCard.defaultProps = {
+  bg: "white",
+  height: "256px",
+  borderWidth: 0,
+  boxShadowSize: "lg",
+  mt: 1
+};
+
+const MenuRoot = React.forwardRef((props, ref) => (
+  <MenuCard {...props} innerRef={ref}/>
+));
+
+export const MenuAuto = ({ children, ...props }) => (
+  <AutocompleteContext.Consumer
+    children={({ match, isOpen, getInputProps, inputValue }) => (
+      <MenuRoot
+        {...getInputProps({
+          ...props,
+          isOpen
+        })}
+        children={
+          isOpen
+            ? React.Children.toArray(children)
+              .filter(el => match(el.props.item, inputValue))
+              .map((el, index) => React.cloneElement(el, { index }))
+            : false
+        }
+      />
+    )}
+  />
+);
+
+const ItemRoot = styled(Flex)`
+  cursor: pointer;
+  &[aria-selected='true'] {
+    background-color: ${themeGet("colors.lightBlue")};
+  }
+  &[data-highlighted] {
+    color: ${themeGet("colors.white")};
+    background-color: ${themeGet("colors.blue")};
+    & svg {
+      color: ${themeGet("colors.white")};
+    }
+  }
+`;
+
+ItemRoot.defaultProps = {
+  alignItems: "center"
+};
+
+export const ItemAuto = props => (
+  <AutocompleteContext.Consumer
+    children={({ item, getItemProps, highlightedIndex }) => (
+      <ItemRoot
+        {...getItemProps({
+          item,
+          px: 2,
+          py: 1,
+          "data-highlighted":
+            highlightedIndex === props.index ? true : undefined,
+          ...props
+        })}
+      />
+    )}
+  />
+);
+
+export class Autocomplete extends React.Component {
+  static Label = LabelAuto;
+  static Input = InputAuto;
+  static Menu = MenuAuto;
+  static Item = ItemAuto;
+
+  static propTypes = {
+    match: PropTypes.func
+  };
+
+  static defaultProps = {
+    match: () => true
+  };
+
+  render() {
+    const { children, style, ...props } = this.props;
+
+    return (
+      <Downshift
+        {...props}
+        children={state => (
+          <div style={{ position: "relative", ...style }}>
+            <AutocompleteContext.Provider
+              value={{ ...props, ...state }}
+              children={children}
+            />
+          </div>
+        )}
+      />
+    );
+  }
+}
