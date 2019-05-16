b341082f69e365fa6a74a1eb5a800293d2e654d8
Nick Do <nkhl_linh@yahoo.com>
2019-05-16 16:12:51 +1200

    MINOR CHANGE: added slider with styles

--- src/index.js
+++ src/index.js
@@ -42,6 +42,7 @@ export { default as Modal } from './Modal'
 export { default as SmallModalHeader } from './SmallModalHeader'
 export { default as ModalHeader } from './ModalHeader'
 export { default as ScrollLock } from './ScrollLock'
+export { default as Slider } from './Slider'
 
 export { default as theme } from "./theme";
 export { default as ThemeProvider } from "./ThemeProvider";
